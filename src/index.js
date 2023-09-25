b70cab099e4f77ae85fbc1cd39256e6e0e580d6f
ArchanaNambiar <archanacnambiar@gmail.com>
2023-09-25 15:49:20 +0530

    submitting code-Mon Sep 25 2023 15:49:17 GMT+0530 (India Standard Time)

--- src/index.js
+++ src/index.js
@@ -1,5 +1,56 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-
+import React, { useState, useEffect } from 'react';
 ReactDOM.render(<App />, document.getElementById("root"));
+
+
+
+function GolfGame() {
+  const [ballPosition, setBallPosition] = useState({ left: 0 });
+  const [gameStarted, setGameStarted] = useState(false);
+
+  useEffect(() => {
+    const handleKeyDown = (event) => {
+      if (event.key === 'ArrowRight') {
+        setBallPosition((prevPosition) => ({
+          left: prevPosition.left + 5,
+        }));
+      }
+    };
+
+    if (gameStarted) {
+      window.addEventListener('keydown', handleKeyDown);
+    } else {
+      window.removeEventListener('keydown', handleKeyDown);
+    }
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [gameStarted]);
+
+  const handleStartClick = () => {
+    setGameStarted(true);
+  };
+
+  return (
+    <div>
+      {gameStarted ? (
+        <div
+          className="ball"
+          style={{
+            position: 'absolute',
+            left: `${ballPosition.left}px`,
+          }}
+        />
+      ) : (
+        <button className="start" onClick={handleStartClick}>
+          Start
+        </button>
+      )}
+    </div>
+  );
+}
+
+export default GolfGame;
